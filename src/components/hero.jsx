4d8f518ab1ba9de473e034def048be035e396058
victoromero21 <victorromeroah@unimagdalena.edu.co>
2023-08-31 23:12:34 -0500

    Botones de resumen y registrarse removido

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -8,14 +8,14 @@ export const Hero = () => {
             <img src={backgroundHeroImage} alt="Segundo encuentro Colombiano de limnologia" className="hero-background-img" />
         </div>
 
-        <section className="hero-buttons">
+        {/* <section className="hero-buttons">
             <a className="hero-button hero-button-register" href='https://docs.google.com/forms/d/e/1FAIpQLSczW5VbGaNR0wtQJKv4pz0CZ4uXvPummIdZ8QD2pIaBOgCTsg/viewform' target='_blank' rel='noreferrer'>
                 Registrarse
             </a>
             <a className="hero-button hero-button-resumenes" href='https://docs.google.com/forms/d/e/1FAIpQLSedzCPnyzfBEKGaAE-yXlgyy7pcqpww7A2czxZBg_1IkJZxdQ/viewform' target='_blank' rel='noreferrer'>
                 Resumenes
             </a>
-        </section>
+        </section> */}
         <a href="https://goo.gl/maps/tgFFUcCpKMAb7fTB9" target='_blank' rel="noreferrer" className="hero-location">
             <GrMapLocation />
             Universidad del Magdalena
