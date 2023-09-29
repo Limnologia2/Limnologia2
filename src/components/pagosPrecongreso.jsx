5a1fbcc6d786e3c20b9eb881d91dc3a9bfc2f1eb
Limnologia2 <encuentrolimnocol2@gmail.com>
2023-09-29 01:16:41 -0500

    Cambios de precongreso

--- src/components/pagosPrecongreso.jsx
+++ src/components/pagosPrecongreso.jsx
@@ -1,18 +1,23 @@
 import { Payments } from "../helpers/pagos"
+import FechaHora from "../assets/Pagos/fechaHora.png"
 export const PagosPrecongreso = () => {
-    const paymentsRender = Payments.filter((payment) =>{
+    const paymentsRender = Payments.filter((payment) => {
         return payment.tipo === "preencuentro"
     })
     return (
         <section className="pagos" id="pagos">
             <h2 className="pagos-title">Cursos pre-encuentro</h2>
             <div className="container">
+                <p>
+
+                    <img src={FechaHora} alt="Fecha y hora del encuentro" className="pagos-fecha" />
+                </p>
                 <ul className="pagos-list">
                     {paymentsRender && paymentsRender.map((payment, index) => {
                         return (
                             <li key={index} className="pagos-item">
                                 <a href={payment.link} className="pagos-item-link" target="_blank" rel="noreferrer">
-                                    <img src={payment.image} alt="pagos" className="pagos-item-image" />
+                                    <img src={payment.image} alt="pagos" className={`pagos-item-image${payment.tipo == "normal" ? "" : " preencuentro"}`} />
                                     <h3 className="pagos-item-title">
                                         {payment.name}
                                     </h3>
